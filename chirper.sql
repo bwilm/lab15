6ac389e37c950beaaddabd2a22f39ff02e706d4b
Brandon Wilmott <Bran@Brandons-MacBook.local>
2017-10-23 17:16:19 -0500

    mysql dump

---
+++ chirper.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: ChirperDatabase
+-- ------------------------------------------------------
+-- Server version	5.7.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CHIRPS`
+--
+
+DROP TABLE IF EXISTS `CHIRPS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CHIRPS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(140) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CHIRPS`
+--
+
+LOCK TABLES `CHIRPS` WRITE;
+/*!40000 ALTER TABLE `CHIRPS` DISABLE KEYS */;
+INSERT INTO `CHIRPS` VALUES (1,'Hello World!','Brandon','2017-10-23 20:51:22'),(2,'I love cheese!','Claudio','2017-10-23 20:53:27'),(3,'I LOVE TRAFFIC','SERGIO','2017-10-23 20:56:34'),(4,'IM INJURED AGAIN','PAUL','2017-10-23 20:57:37'),(5,'Back from injury already','Zlatan','2017-10-23 20:58:28'),(6,'DAB DAB DAB DAB','Paul','2017-10-23 20:59:04'),(7,'I cant cross a ball for sh*t','Daley','2017-10-23 20:59:56'),(8,'Im a dab merchant','Jesse','2017-10-23 21:01:08'),(9,'Im godawful','Ander','2017-10-23 21:03:13'),(10,'I am french and speak no english','Anthony','2017-10-23 21:04:02');
+/*!40000 ALTER TABLE `CHIRPS` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-23 17:09:31
